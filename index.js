ad2c6dd6890a14607c51ea77b791f2f5f06e3ed3
Belchior <belchior.fontao@gmail.com>
2022-01-22 15:20:56 +0000

    test

--- index.js
+++ index.js
@@ -220,3 +220,341 @@ function gameOver() {
   theTrash2 = [];
   alert("Game Over!!! You are not ready to help me! Silly human!");
 }
+
+
+const imageTrash = new Image();
+imageTrash.src = "/bottle.png";
+
+const imageTrash2 = new Image();
+imageTrash2.src = "/medical_mask_PNG25.png";
+
+let theTrash = [];
+let frames = 0;
+
+let trashIds = 1;
+
+class Trash {
+  constructor(argX, argY, argWidth, argHeight, argID) {
+    this.x = argX;
+    this.y = argY;
+    this.width = argWidth;
+    this.height = argHeight;
+    this.speed = -1;
+    this.id = argID;
+  }
+
+  move() {
+    this.x += this.speed;
+    if (time <= 150 && time >= 126) {
+      this.speed = -1;
+    }
+    if (time <= 125 && time >= 100) {
+      this.speed = -1.6;
+    }
+    if (time <= 99 && time >= 60) {
+      this.speed = -2.35;
+    }
+    if (time <= 59 && time >= 40) {
+      this.speed = -3.35;
+    }
+    if (time <= 39 && time >= 15) {
+      this.speed = -4.1;
+    }
+    if (time <= 14 && time >= 1) {
+      this.speed = -4.6;
+    }
+  }
+
+  draw() {
+    this.move(),
+      ctx.drawImage(imageTrash, this.x, this.y, this.width, this.height);
+  }
+}
+
+function trashAppears() {
+  theTrash.forEach((trash) => {
+    trash.draw();
+  });
+  const randomX = randomIntFromInterval(10, 60);
+  const trashPositionX = canvas.width + randomX;
+  if (frames % 100 === 0 && time <= 150 && time >= 126) {
+    const newT = randomIntFromInterval(40, 400);
+    theTrash.push(new Trash(trashPositionX, newT, 50, 50, trashIds));
+    trashIds++;
+  }
+
+  if (frames % 50 === 0 && time <= 125 && time > 0) {
+    const newT = randomIntFromInterval(40, 400);
+    theTrash.push(new Trash(trashPositionX, newT, 50, 50, trashIds));
+    trashIds++;
+  }
+}
+
+// adding the second trash
+
+let theTrash2 = [];
+
+let trashIds2 = 1;
+
+class Trash2 {
+  constructor(argX, argY, argWidth, argHeight, argID) {
+    this.x = argX;
+    this.y = argY;
+    this.width = argWidth;
+    this.height = argHeight;
+    this.speed = -1;
+    this.id = argID;
+  }
+
+  move() {
+    this.x += this.speed;
+    if (time <= 150 && time >= 126) {
+      this.speed = -1.1;
+    }
+    if (time <= 125 && time >= 100) {
+      this.speed = -1.7;
+    }
+    if (time <= 99 && time >= 60) {
+      this.speed = -2.45;
+    }
+    if (time <= 59 && time >= 40) {
+      this.speed = -3.45;
+    }
+    if (time <= 39 && time >= 15) {
+      this.speed = -4.2;
+    }
+    if (time <= 14 && time >= 1) {
+      this.speed = -4.7;
+    }
+  }
+
+  draw() {
+    this.move(),
+      ctx.drawImage(imageTrash2, this.x, this.y, this.width, this.height);
+  }
+}
+
+function trashAppears2() {
+  theTrash2.forEach((trash) => {
+    trash.draw();
+  });
+  const randomX = randomIntFromInterval(10, 60);
+  const trashPositionX = canvas.width + randomX;
+  if (frames % 100 === 0 && time <= 150 && time >= 126) {
+    const newT = randomIntFromInterval(40, 400);
+    theTrash2.push(new Trash2(trashPositionX, newT, 50, 50, trashIds2));
+    trashIds2++;
+  }
+  if (frames % 50 === 0 && time <= 125 && time > 0) {
+    const newT = randomIntFromInterval(40, 400);
+    theTrash2.push(new Trash2(trashPositionX, newT, 50, 50, trashIds2));
+    trashIds2++;
+  }
+}
+
+
+
+
+
+
+// fish 
+
+const imageFish1 = new Image();
+imageFish1.src = "/cartoon_fish_06_yellow_swim.png";
+
+const imageFish2 = new Image();
+imageFish2.src = "/cartoon_fish_06_blue_swim.png";
+
+//adding the fish
+
+let theFish = [];
+
+let theFishIds = 1;
+
+class Fish {
+  constructor(argX, argY, argWidth, argHeight, argID) {
+    this.x = argX;
+    this.y = argY;
+    this.width = argWidth;
+    this.height = argHeight;
+    this.speed = -0.9;
+    this.id = argID;
+    this.frame = 0;
+    this.frameX = 0;
+    this.frameY = 0;
+
+    this.fishImgWidth = 498;
+    this.fishImgHeight = 327;
+  }
+
+  move() {
+    this.x += this.speed;
+    if (time <= 150 && time >= 126) {
+      this.speed = -0.9;
+    }
+    if (time <= 125 && time >= 100) {
+      this.speed = -1.4;
+    }
+    if (time <= 99 && time >= 60) {
+      this.speed = -2.15;
+    }
+    if (time <= 59 && time >= 40) {
+      this.speed = -3.15;
+    }
+    if (time <= 39 && time >= 15) {
+      this.speed = -3.9;
+    }
+    if (time <= 14 && time >= 1) {
+      this.speed = -4.4;
+    }
+
+    if (frames % 5 === 0) {
+      this.frame++;
+      if (this.frame >= 12) this.frame = 0;
+      if (this.frame === 3 || this.frame === 7 || this.frame === 11) {
+        this.frameX = 0;
+      } else {
+        this.frameX++;
+      }
+      if (this.frame < 3) this.frameY = 0;
+      else if (this.frame < 7) this.frameY = 1;
+      else if (this.frame < 11) this.frameY = 2;
+      else this.frameY = 0;
+    }
+  }
+
+  draw() {
+    this.move(),
+      ctx.drawImage(
+        imageFish1,
+        this.frameX * this.fishImgWidth,
+        this.frameY * this.fishImgHeight,
+        this.fishImgWidth,
+        this.fishImgHeight,
+        this.x,
+        this.y,
+        this.width,
+        this.height
+      );
+  }
+}
+
+function fishAppears() {
+  theFish.forEach((fish) => {
+    fish.draw();
+  });
+  const randomX = randomIntFromInterval(10, 60);
+  const fishPositionX = canvas.width + randomX;
+
+  if (frames % 100 === 0 && time <= 150 && time >= 126) {
+    const newY = randomIntFromInterval(40, 400);
+    theFish.push(new Fish(fishPositionX, newY, 50, 50, theFishIds));
+
+    theFishIds++;
+  }
+  if (frames % 50 === 0 && time <= 125 && time > 0) {
+    const newY = randomIntFromInterval(40, 400);
+    theFish.push(new Fish(fishPositionX, newY, 50, 50, theFishIds));
+
+    theFishIds++;
+  }
+}
+
+// adding the Fish2
+
+let theFish2 = [];
+
+let theFishIds2 = 1;
+
+class Fish2 {
+  constructor(argX, argY, argWidth, argHeight, argID) {
+    this.x = argX;
+    this.y = argY;
+    this.width = argWidth;
+    this.height = argHeight;
+    this.speed = -0.9;
+    this.id = argID;
+    this.frame = 0;
+    this.frameX = 0;
+    this.frameY = 0;
+
+    this.fishImgWidth = 498;
+    this.fishImgHeight = 327;
+  }
+
+  move() {
+    this.x += this.speed;
+    if (time <= 150 && time >= 126) {
+      this.speed = -0.8;
+    }
+    if (time <= 125 && time >= 100) {
+      this.speed = -1.3;
+    }
+    if (time <= 99 && time >= 60) {
+      this.speed = -2.05;
+    }
+    if (time <= 59 && time >= 40) {
+      this.speed = -3.05;
+    }
+    if (time <= 39 && time >= 15) {
+      this.speed = -3.8;
+    }
+    if (time <= 14 && time >= 1) {
+      this.speed = -4.3;
+    }
+
+    if (frames % 5 === 0) {
+      this.frame++;
+      if (this.frame >= 12) this.frame = 0;
+      if (this.frame === 3 || this.frame === 7 || this.frame === 11) {
+        this.frameX = 0;
+      } else {
+        this.frameX++;
+      }
+      if (this.frame < 3) this.frameY = 0;
+      else if (this.frame < 7) this.frameY = 1;
+      else if (this.frame < 11) this.frameY = 2;
+      else this.frameY = 0;
+    }
+  }
+
+  draw() {
+    this.move(),
+      ctx.drawImage(
+        imageFish2,
+        this.frameX * this.fishImgWidth,
+        this.frameY * this.fishImgHeight,
+        this.fishImgWidth,
+        this.fishImgHeight,
+        this.x,
+        this.y,
+        this.width,
+        this.height
+      );
+  }
+}
+
+function fishAppears2() {
+  theFish2.forEach((fish2) => {
+    fish2.draw();
+  });
+  const randomX = randomIntFromInterval(10, 60);
+  const fishPositionX = canvas.width + randomX;
+
+  if (frames % 100 === 0 && time <= 150 && time >= 126) {
+    const newY = randomIntFromInterval(40, 400);
+    theFish2.push(new Fish2(fishPositionX, newY, 50, 50, theFishIds2));
+    theFishIds2++;
+  }
+  if (frames % 100 === 0 && time <= 125 && time > 0) {
+    const newY = randomIntFromInterval(40, 400);
+    theFish2.push(new Fish2(fishPositionX, newY, 50, 50, theFishIds2));
+    theFishIds2++;
+  }
+}
+
+//that random trick
+
+function randomIntFromInterval(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
